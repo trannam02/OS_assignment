c0b754c6917099264c727bac8ccad421bd712b5c
phat-tech123 <caovinhphat1904@gmail.com>
2024-12-11 00:23:47 +0700

    fix sched

--- src/queue.c
+++ src/queue.c
@@ -7,26 +7,50 @@ int empty(struct queue_t * q) {
 	return (q->size == 0);
 }
 void enqueue(struct queue_t *q, struct pcb_t *proc) {
-    if (q == NULL || proc == NULL) return; 
+   	if (q == NULL || proc == NULL) return; 
 
-    if (q->size >= MAX_QUEUE_SIZE) {
-        return;
-    }
-    q->proc[q->size] = proc;
-    q->size++;
+    	if (q->size >= MAX_QUEUE_SIZE) {
+        	return;
+    	}
+    	q->proc[q->size] = proc;
+    	q->size++;
 }
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose priority is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	if (q == NULL || empty(q)) return NULL;
-
-	struct pcb_t *proc = q->proc[0]; 
-	for (int i = 0; i < q->size - 1; i++) {
-	q->proc[i] = q->proc[i + 1]; 
+	int sizeOfQueue = q->size;
+	if( sizeOfQueue ==1 ){
+		struct pcb_t* return_proc = q->proc[0];
+		q->proc[0]=NULL;
+		q->size =0;
+		return return_proc;
 	}
-	q->size--;
-	return proc;
+	else if( sizeOfQueue >1 && sizeOfQueue <= MAX_QUEUE_SIZE){
+		struct pcb_t* return_proc= q->proc[0];// process will be returned
+		uint32_t highest_prior = q->proc[0]->priority;
+		int position =0;
+		int i;
+		for(i=0 ; i < sizeOfQueue ;i++){
+			if( highest_prior < q->proc[i]->priority ){
+				highest_prior =q->proc[i]->priority;
+				position = i;
+			}	
+		}
+		return_proc = q->proc[position];
+		if(position == (sizeOfQueue -1 )){
+			q->proc[position] =NULL;
+			q->size = sizeOfQueue -1;
+			return return_proc;
+		}
+		else {
+			for(i = position+1;i< sizeOfQueue; i++){
+				q->proc[i-1]=q->proc[i];
+			}
+			q->proc[sizeOfQueue -1]=NULL;
+			q->size = sizeOfQueue -1;
+			return return_proc;
+		}	
+	}
+	return NULL;
 }
-
-
